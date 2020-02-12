2312806809f3488682305464a6ce73f8455e0c39
Fan Syong Yue <mastersyong@gmail.com>
2020-02-12 16:58:15 +0800

    add vue socket io extended

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import Notifications from "vue-notification";
+import VueSocketIOExt from "vue-socket.io-extended";
+import io from "socket.io-client";
 
+const socket = io("http://192.168.43.168:8080");
+Vue.use(VueSocketIOExt, socket);
 Vue.use(Notifications);
 Vue.config.productionTip = false;
 import "@/assets/bulma.min.css";
