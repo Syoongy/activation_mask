be448ec807f060610cb198cf66a19c7e73152bfc
Fan Syong Yue <mastersyong@gmail.com>
2020-02-20 16:47:37 +0800

    Scoreboard component

---
+++ src/components/Scoreboard/Board.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="board">
+    <Tile
+      v-for="station in station"
+      :key="station.stationNo"
+      :station="station"
+    />
+  </div>
+</template>
+
+<script>
+import Tile from "./Tile";
+export default {
+  components: {
+    Tile
+  },
+  props: {
+    stations: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped></style>
