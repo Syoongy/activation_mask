a3d0366766efed8d418c6c7cd3b9a952ce663db2
Fan Syong Yue <mastersyong@gmail.com>
2020-02-20 16:44:53 +0800

    LeaderboardTile component -Update styling

--- src/components/Leaderboard/LeaderboardTile.vue
+++ src/components/Leaderboard/LeaderboardTile.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="leaderboard-tile has-text-white">
+  <div class="leaderboard-tile">
     <div class="content columns">
-      <p class="column is-3">No. {{ rank }}</p>
-      <p class="column is-6">Station {{ name }}</p>
-      <p class="column">{{ qty }}/200</p>
+      <p class="column is-size-4 is-3 has-text-weight-bold">No. {{ rank }}</p>
+      <p class="column is-size-4 is-6 has-text-weight-bold">
+        Station {{ name }}
+      </p>
+      <p class="column is-size-4 has-text-weight-bold">{{ qty }}/18</p>
     </div>
   </div>
 </template>
@@ -30,7 +32,6 @@ export default {
 <style>
 .leaderboard-tile {
   margin: 0.75rem;
-  background-color: #393e46;
   border-radius: 5px;
   padding: 1rem;
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.23), 0 6px 6px rgba(0, 0, 0, 0.27);
