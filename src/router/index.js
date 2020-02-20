aa276ef865a76f29741bae7541e2dd2504c117cd
Fan Syong Yue <mastersyong@gmail.com>
2020-02-20 16:45:20 +0800

    Styling

--- src/router/index.js
+++ src/router/index.js
@@ -7,26 +7,22 @@ const routes = [
   {
     path: "/",
     name: "dashboard",
-    component: () =>
-      import(/* webpackChunkName: "home" */ "../views/Dashboard.vue")
+    component: () => import(/* webpackChunkName: "home" */ "../views/Dashboard")
   },
   {
     path: "/finished",
     name: "finished",
-    component: () =>
-      import(/* webpackChunkName: "home" */ "../views/Finished.vue")
+    component: () => import(/* webpackChunkName: "home" */ "../views/Finished")
   },
   {
     path: "/received",
     name: "received",
-    component: () =>
-      import(/* webpackChunkName: "home" */ "../views/Received.vue")
+    component: () => import(/* webpackChunkName: "home" */ "../views/Received")
   },
   {
     path: "/shipped",
     name: "shipped",
-    component: () =>
-      import(/* webpackChunkName: "home" */ "../views/Shipped.vue")
+    component: () => import(/* webpackChunkName: "home" */ "../views/Shipped")
   },
   {
     path: "/about/:id",
@@ -34,8 +30,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/About")
+  },
+  {
+    path: "/statistics/quantity",
+    name: "quantity",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "home" */ "../views/statistics/Quantity")
   }
 ];
 
