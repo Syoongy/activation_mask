fac0e7473bf7cd251e6779c56bacd06e6887a73f
Fan Syong Yue <mastersyong@gmail.com>
2020-02-20 16:46:31 +0800

    Quantity vue and component

---
+++ src/components/Statistics/Quantity.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container columns is-multiline qty-container is-centered">
+    <p class="title has-text-centered column is-12" :class="classObject">
+      {{ title }}
+    </p>
+    <div class="box has-background-black-ter column has-text-centered">
+      <p class="has-text-weight-bold is-size-3" :class="classObject">Masks</p>
+      <p class="is-size-4" :class="classObject">{{ masks }}</p>
+    </div>
+    <!-- <div class="box has-background-black-ter column has-text-centered">
+      <p class="has-text-weight-bold is-size-3" :class="classObject">
+        Thermometers
+      </p>
+      <p class="is-size-4" :class="classObject">{{ thermometers }}</p>
+    </div>
+    <div class="box has-background-black-ter column has-text-centered">
+      <p class="has-text-weight-bold is-size-3" :class="classObject">
+        Sanitisers
+      </p>
+      <p class="is-size-4" :class="classObject">{{ sanitisers }}</p>
+    </div> -->
+    <div class="box has-background-black-ter column has-text-centered">
+      <p class="has-text-weight-bold is-size-3" :class="classObject">
+        Ziplocs
+      </p>
+      <p class="is-size-4" :class="classObject">{{ ziplocs }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    masks: {
+      type: Number,
+      required: true
+    },
+    thermometers: {
+      type: Number,
+      required: true
+    },
+    sanitisers: {
+      type: Number,
+      required: true
+    },
+    ziplocs: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    classObject() {
+      return {
+        "has-text-warning": this.title === "Received",
+        "has-text-danger": this.title !== "Received"
+      };
+    }
+  }
+};
+</script>
+
+<style>
+.qty-container {
+  margin: 1rem !important;
+}
+.qty-container .box.column {
+  margin: 0.75rem !important;
+}
+</style>
---
+++ src/views/statistics/Quantity.vue
@@ -0,0 +1,120 @@
+<template>
+  <section id="quantity-section">
+    <div class="is-flex" id="quantity-container">
+      <div class="box has-background-black-ter box-spacing">
+        <Quantity
+          title="Received"
+          :masks="receivedItems.mask"
+          :thermometers="receivedItems.thermometer"
+          :sanitisers="receivedItems.sanitiser"
+          :ziplocs="receivedItems.ziploc"
+        ></Quantity>
+        <Quantity
+          title="Remaining"
+          :masks="remainingItems.mask"
+          :thermometers="remainingItems.thermometer"
+          :sanitisers="remainingItems.sanitiser"
+          :ziplocs="remainingItems.ziploc"
+        ></Quantity>
+      </div>
+      <div class="box has-background-black-ter box-spacing">
+        <ProductTracking
+          :shipped="shipped"
+          :delivered="delivered"
+          :finished="finishedBoxes"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Quantity from "@/components/Statistics/Quantity";
+import ProductTracking from "@/components/Statistics/ProductTracking";
+import ky from "ky";
+
+export default {
+  components: {
+    Quantity,
+    ProductTracking
+  },
+  sockets: {
+    receivedValues(val) {
+      const data = val.Item;
+      this.receivedItems.mask = data.mask;
+      this.receivedItems.thermometer = data.thermometer;
+      this.receivedItems.sanitiser = data.handSanitiser;
+      this.receivedItems.ziploc = data.ziploc;
+    },
+    finishedValues(val) {
+      let newFinishedBoxesValues = 0;
+      for (const section of val) {
+        newFinishedBoxesValues += section.quantity;
+      }
+      this.finishedBoxes = newFinishedBoxesValues;
+    }
+  },
+  computed: {
+    remainingItems() {
+      const retObj = { ...this.receivedItems };
+      retObj.mask = retObj.mask - this.finishedBoxes * this.boxMask;
+      retObj.thermometer =
+        retObj.thermometer - this.finishedBoxes * this.boxThermometer;
+      retObj.sanitiser =
+        retObj.sanitiser - this.finishedBoxes * this.boxSanitiser;
+      retObj.ziploc = retObj.ziploc - this.finishedBoxes * this.boxZiploc;
+      return retObj;
+    }
+  },
+  data() {
+    return {
+      receivedItems: { mask: 0, thermometer: 0, sanitiser: 0, ziploc: 0 },
+      finishedBoxes: 0,
+      boxMask: 1600,
+      boxThermometer: 0,
+      boxSanitiser: 0,
+      boxZiploc: 400,
+      shipped: 0,
+      delivered: 0
+    };
+  },
+  async mounted() {
+    let res = await ky.get("http://54.169.249.3:8080/getAllReceived").json();
+    const data = res.Item;
+    this.receivedItems.mask = data.mask;
+    this.receivedItems.thermometer = data.thermometer;
+    this.receivedItems.sanitiser = data.handSanitiser;
+    this.receivedItems.ziploc = data.ziploc;
+    res = await ky.get("http://54.169.249.3:8080/getFinished").json();
+    for (const section of res) {
+      this.finishedBoxes += section.quantity;
+    }
+    res = await ky.get("http://54.169.249.3:8080/totalShipped").json();
+    this.shipped = res[0].totalBoxes;
+  }
+};
+</script>
+
+<style>
+.box:not(:last-child) {
+  margin-bottom: 0;
+}
+
+#quantity-section {
+  width: 100vw;
+  min-height: 100vh;
+  overflow: hidden;
+}
+
+#quantity-container {
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.box-spacing {
+  margin-bottom: 1rem !important;
+  width: 80%;
+}
+</style>
