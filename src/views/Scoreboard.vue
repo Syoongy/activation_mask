aec3650db4644e2e961eebd505789c174145f446
Fan Syong Yue <mastersyong@gmail.com>
2020-02-20 16:48:24 +0800

    Scoreboard view

---
+++ src/views/Scoreboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <Board />
+    </div>
+  </div>
+</template>
+
+<script>
+import Board from "@/components/Scoreboard/Board";
+export default {
+  components: {
+    Board
+  }
+};
+</script>
+
+<style scoped></style>
