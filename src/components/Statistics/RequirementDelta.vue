fbd64b2457cd90cba8ff70bbc341dd161341b909
Matthew <mzw17@ic.ac.uk>
2020-02-22 01:22:58 +0800

    Added current requirement (live) and current delta to ops board

---
+++ src/components/Statistics/RequirementDelta.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    id="tracking-container"
+    class="container columns is-multiline is-centered"
+  >
+    <div
+      class="column box has-background-black-ter has-text-centered has-text-white"
+    >
+      <p class="has-text-weight-bold is-size-1">Delta on Target</p>
+      <p class="has-text-weight-bold is-size-1">{{ delta }}</p>
+    </div>
+    <!-- <div
+      class="column box has-background-black-ter has-text-centered has-text-success"
+    >
+      <p class="has-text-weight-bold is-size-3">Delivered</p>
+      <p class="is-size-4">{{ delivered }}</p>
+    </div> -->
+    <!-- <div
+      class="column box has-background-black-ter has-text-centered has-text-warning"
+    >
+      <p class="has-text-weight-bold is-size-3">Current Required Packs</p>
+      <p class="is-size-4">{{ currentRequiredPacks }}</p>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    delta: {
+      type: Number,
+      required: true
+    }
+  },
+  sockets: {
+    shippedValues(val) {
+      this.shipped = val[0].totalBoxes;
+    }
+  }
+};
+</script>
+
+<style>
+#tracking-container {
+  margin: auto;
+}
+#tracking-container .box.column {
+  margin: 0.75rem !important;
+}
+</style>
