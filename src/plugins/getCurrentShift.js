2ae7be067252a328f92bbef59b4c20ab711a1911
Fan Syong Yue <mastersyong@gmail.com>
2020-02-21 22:26:43 +0800

    get the current shift based on new Date()

---
+++ src/plugins/getCurrentShift.js
@@ -0,0 +1,34 @@
+export default function() {
+  const d = new Date();
+  console.log(d);
+  let session = "";
+  let currentDate = d.getDate();
+  let currentHour = d.getHours();
+  switch (currentDate) {
+    case 24:
+      if (currentHour >= 16) {
+        session = "2";
+      } else {
+        session = "1";
+      }
+      break;
+    case 25:
+      if (currentHour >= 16) {
+        session = "4";
+      } else {
+        session = "3";
+      }
+      break;
+    case 26:
+      if (currentHour >= 16) {
+        session = "6";
+      } else {
+        session = "5";
+      }
+      break;
+    default:
+      session = "1";
+      break;
+  }
+  return session;
+}
