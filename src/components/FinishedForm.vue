df53da63003f5cae5eb0edb36ec76c92cc8a7f19
Fan Syong Yue <mastersyong@gmail.com>
2020-02-21 16:44:49 +0800

    add session and append

--- src/components/FinishedForm.vue
+++ src/components/FinishedForm.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="container">
+    <div class="field">
+      <label class="label">Session</label>
+      <div class="field">
+        <div class="control">
+          <div class="select">
+            <select name="session" v-model="session">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
     <label class="label">Table</label>
     <div class="field is-grouped">
-      <!-- <div class="control">
-        <input
-          type="number"
-          :class="{ input: true, 'is-danger': !isNameCorrect }"
-          @input="validateName"
-          placeholder="Enter table here"
-          v-model.number="name"
-          maxlength="1"
-        />
-      </div> -->
-      <div class="control">
-        <div class="select">
+      <div class="control is-expanded">
+        <div class="select is-fullwidth">
           <select name="stationAlpha" v-model="sAlpha">
             <option value="A">A</option>
             <option value="B">B</option>
@@ -21,16 +28,11 @@
             <option value="D">D</option>
             <option value="E">E</option>
             <option value="F">F</option>
-            <option value="G">G</option>
-            <option value="H">H</option>
-            <option value="I">I</option>
-            <option value="J">J</option>
-            <option value="K">K</option>
           </select>
         </div>
       </div>
-      <div class="control">
-        <div class="select">
+      <div class="control is-expanded">
+        <div class="select is-fullwidth">
           <select name="stationNum" v-model="sNum">
             <option value="1">1</option>
             <option value="2">2</option>
@@ -39,25 +41,8 @@
             <option value="5">5</option>
             <option value="6">6</option>
             <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
           </select>
         </div>
-      </div>
-    </div>
-    <div class="field">
-      <label class="label">Quantity</label>
-      <div class="control">
-        <input
-          type="number"
-          :class="{ input: true, 'is-danger': !isQtyCorrect }"
-          @input="validateQty"
-          placeholder="Enter quantity here"
-          v-model.number="qty"
-          step="1"
-          min="1"
-          max="50"
-        />
       </div>
     </div>
     <div class="field">
@@ -84,13 +69,15 @@ export default {
       return this.qty >= 1 && this.qty <= 50;
     },
     name() {
-      return this.sAlpha + this.sNum;
+      const name = this.session + this.sAlpha + this.sNum;
+      return name;
     }
   },
   data() {
     return {
       sAlpha: "A",
       sNum: "1",
+      session: "",
       qty: 1,
       isNameCorrect: true,
       isQtyCorrect: true
@@ -103,16 +90,18 @@ export default {
     async submitForm() {
       //submit to api here
       //Python api
-      const res = await ky.get(
+      await ky.get(
         `http://supplypacking.pythonanywhere.com/add_production/${this.name}/${this.qty}/`
       );
       //Node api
-      await ky.post("http://54.169.249.3:8080/addFinished", {
-        json: {
-          stationNo: this.name,
-          quantity: this.qty
-        }
-      });
+      const res = await ky
+        .post("http://54.169.249.3:8080/addFinished", {
+          json: {
+            stationNo: this.name,
+            quantity: this.qty
+          }
+        })
+        .json();
       this.$socket.client.emit("addToStationNo", this.name);
       this.name = null;
       this.qty = 1;
@@ -120,8 +109,8 @@ export default {
         group: "submitReq",
         type: "my-success",
         title: "Success!",
-        text: "Input has been submitted!",
-        duration: 1000,
+        text: `The ID is ${res.uniqueId}`,
+        duration: -1000,
         max: 1
       });
     },
