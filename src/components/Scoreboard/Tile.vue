ab24c8cd1e461df3168c632cf4a296991df251ac
Fan Syong Yue <mastersyong@gmail.com>
2020-02-21 16:46:49 +0800

    Tile component

---
+++ src/components/Scoreboard/Tile.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="box" :class="classObject">
+    <p
+      class="has-text-weight-bold is-size-1 has-text-centered"
+      v-if="!station.isStation"
+    >
+      {{ station.stationName }}
+    </p>
+    <p
+      class="is-size-1 has-text-centered has-text-weight-bold has-text-white"
+      v-else
+    >
+      {{ station.quantity }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    station: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    classObject() {
+      let styling = "";
+      if (this.station.isStation) {
+        switch (this.station.letter) {
+          case "A":
+            styling = "has-background-a";
+            break;
+          case "B":
+            styling = "has-background-b";
+            break;
+          case "C":
+            styling = "has-background-c";
+            break;
+          case "D":
+            styling = "has-background-d";
+            break;
+          case "E":
+            styling = "has-background-danger";
+            break;
+          case "F":
+            styling = "has-background-black has-text-white";
+            break;
+          case "G":
+            styling = "has-background-success";
+            break;
+          default:
+            break;
+        }
+      } else {
+        styling = "has-background-grey has-text-white";
+      }
+      return styling;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.is-size-1 {
+  line-height: 4.5rem !important;
+  font-size: 5rem !important;
+}
+
+.box {
+  padding: 0.4rem;
+  margin-bottom: 1rem;
+}
+
+.has-background-a {
+  background-color: #12005e !important;
+}
+
+.has-background-b {
+  background-color: #003d00 !important;
+}
+
+.has-background-c {
+  background-color: #002171 !important;
+}
+
+.has-background-d {
+  background-color: #ac1900 !important;
+}
+</style>
