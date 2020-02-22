f7cebfe0ffeef1a34f8f93ce85613f55554cd609
Matthew <mzw17@ic.ac.uk>
2020-02-22 18:05:33 +0800

    Added new socket variable for safti in main.js

--- src/main.js
+++ src/main.js
@@ -5,8 +5,12 @@ import Notifications from "vue-notification";
 import VueSocketIOExt from "vue-socket.io-extended";
 import io from "socket.io-client";
 
+// PLC IP
 const socket = io("http://54.169.249.3:8080");
+// SAFTI IP
+const socket1 = io("http://54.254.221.3:8080");
 Vue.use(VueSocketIOExt, socket);
+Vue.use(VueSocketIOExt, socket1);
 Vue.use(Notifications);
 Vue.config.productionTip = false;
 import "@/assets/bulma.min.css";
